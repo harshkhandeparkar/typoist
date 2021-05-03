fd67a6a5f2101d3de8f602282eb45d16c7ae0e84
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-05-03 20:10:30 +0530

    feat: latest version

--- examples/web/index.js
+++ examples/web/index.js
@@ -3,27 +3,28 @@ const typeIt = (selector, text, options) => {
 
   const typoist = new Typoist.Typoist({
     ...options,
-    appendFunction: (character) => {
-      textArea.innerHTML += character;
-    },
-    deleteFunction: () => {
-      textArea.innerText = textArea.innerText.slice(0, -1)
-    }
+    appendFunction: (character) => new Promise((resolve) => {
+      textArea.innerText += character;
+      resolve();
+    }),
+    deleteFunction: () => new Promise((resolve) => {
+      textArea.innerText = textArea.innerText.slice(0, textArea.innerText.length - 1);
+      resolve();
+    })
   })
 
-  typoist.setStringToType(text);
-  typoist.startTyping();
+  typoist.type(text);
 }
 
 const typeFeatures = () => {
-  typeIt('#feat-typo', 'It can make random tpyoes while typing and correct them.', { speed: 15, mistakeProbability: 0 });
-  typeIt('#feat-lots-of-mistakes', 'Lots of typos.', { mistakeProbability: 0.8, speed: 30 });
-  typeIt('#feat-no-mistakes', 'Or no typos at all.', { mistakeProbability: 0, speed: 3 });
+  typeIt('#feat-typo', 'It can make random tpyoes while typing and correct them.', { speed: 10, mistakeProbability: 0, onComplete: () => console.log('done') });
+  typeIt('#feat-lots-of-mistakes', 'Lots of typos.', { mistakeProbability: 0.8, speed: 15, onComplete: () => console.log('done') });
+  typeIt('#feat-no-mistakes', 'Or no typos at all.', { mistakeProbability: 0, speed: 3, onComplete: () => console.log('done') });
   typeIt('#feat-speed', 'Adjustable Speed.', { speed: 1 });
 }
 
 const typeDesc = () => {
-  typeIt('#desc', 'A fun library that can be used to fake fast typing or create typing time-lapses.', { speed: 45, onComplete: typeFeatures });
+  typeIt('#desc', 'A fun library that can be used to fake fast typing or create typing time-lapses.', { speed: 20, onComplete: typeFeatures });
 }
 
 typeIt('#title', 'Typoist', {
